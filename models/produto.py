ff65f951caf73e3c85dffd8dccb4281f10ee2e12
Souza14Lucas <souza14lucas@gmail.com>
2025-11-20 11:37:19 -0300

    refactor: Otimizacao de pastas e limpeza de venv

--- models/produto.py
+++ models/produto.py
@@ -2,12 +2,10 @@ from config.extensions import db
 from sqlalchemy import text
 
 class Produto(db.Model):
-    # __tablename__ deve ser exatamente o nome da tabela criada no init_db.py
     __tablename__ = "produtos" 
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False, unique=True)
     descricao = db.Column(db.Text)
-    # db.Numeric é melhor para dinheiro que db.Float
     preco = db.Column(db.Numeric(10, 2), nullable=False) 
     categoria = db.Column(db.String(50))
     estoque = db.Column(db.Integer, default=0)
@@ -18,7 +16,6 @@ class Produto(db.Model):
         return {
             "id": self.id, 
             "nome": self.nome, 
-            # Converte Numeric para string para JSON, garantindo 2 casas decimais
             "preco": f"{self.preco:.2f}", 
             "descricao": self.descricao,
             "estoque": self.estoque,
